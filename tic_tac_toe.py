b56624ccafea917a13ebaa783008dcca4084a572
wheyisking <92067747+wheyisking@users.noreply.github.com>
2022-01-12 22:39:28 -0700

    Created tic_tac_toe program

---
+++ tic_tac_toe.py
@@ -0,0 +1,111 @@
+"""
+Filename: tic_tac_toe.py
+Author: Shawn Blum
+Purpose: Display a tic tac toe game for the user, show understanding regarding VCS creation
+
+"""
+
+def main():
+
+    player = next_player("")
+
+    board = create_board()
+
+    while not (game_winner(board)) or game_draw(board):
+
+        display_board(board)
+
+        make_move(player,board)
+
+        player = next_player(player)
+
+    display_board(board)
+
+    print('Thanks for playing!')
+
+    return
+
+def create_board():
+
+    board = []
+
+    for square in range(9):
+        board.append(square + 1)
+
+    return board
+
+def display_board(board):
+
+    
+    print(f'\n{board[0]}|{board[1]}|{board[2]}')
+
+    print('-+-+-')
+
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+
+    print('-+-+-')
+
+    print(f"{board[6]}|{board[7]}|{board[8]}\n")
+
+    
+
+def game_draw(board):
+
+    for square in range(9):
+
+        if board[square] != "x" and board[square] != "o":
+            return False
+
+    return True
+
+def game_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
+def make_move(player, board):
+
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+
+    board[square - 1] = player
+
+def next_player(current):
+
+    if current == "" or current == "o":
+        return "x"
+
+    elif current == "x":
+        return "o"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
